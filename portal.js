ee7c5b05a3486a085d93c28199b09b7194550e74
Casey Chu <casey@bitsofpancake.com>
2014-04-03 00:58:25 -0700

    updates

--- portal.js
+++ portal.js
@@ -1,30 +1,50 @@
-
 // Can't be https. (Otherwise, we won't be able to access the API.)
 if (window.location.protocol === 'https:')
 	window.location.replace('http://www.cs.hmc.edu/~cchu/portal/');
 
+// Which page should be shown.
+var router = {
+	'catalog': function () {
+		return 'catalog-browse';
+	},
+
+	'catalog/(\\d{4})/(FA|SU|SP)/([A-Z]{1,5})': function (yr, sess, disc) {
+		document.querySelector('#cat').value = yr + '/' + sess;
+		document.querySelector('#disc').value = disc;
+		api('list/' + yr + '/' + sess + '/' + disc, listCourses);
+		return 'catalog-list';
+	},
+	
+	'schedule': function () {
+		return 'schedule';
+	}
+};
+
 // The main router.
 window.onhashchange = function () {
-	var parts = window.location.hash.substr(1).split('/');
-	switch (parts.shift()) {
-		case 'list':
-			var yr = +parts.shift();
-			var sess = parts.shift();
-			var disc = parts.shift();
-			if (['FA', 'SU', 'SP'].indexOf(sess) == -1 || !/^[A-Z]{1,10}$/.test(disc)) {
-				document.querySelector('#search button').onclick();
-				break;
-			}
-			
-			document.querySelector('#cat').value = yr + '/' + sess;
-			document.querySelector('#disc').value = disc;
-			api('list/' + yr + '/' + sess + '/' + disc, listCourses);
-			return;
+	if (window.location.hash == '#')
+		return;
+	
+	var viewName = false;
+	for (var pattern in router) {
+		window.location.hash.replace(new RegExp('^#' + pattern + '$'), function () {
+			viewName = router[pattern].apply(window, Array.prototype.slice.call(arguments, 1));
+		})
 		
+		// If viewName is "X-Y-Z", this sets <body>'s class to "X-view X-Y-view X-Y-Z-view".
+		if (viewName) {
+			var classes = [];
+			var parts = viewName.split('-');
+			while (parts.length)
+				classes.push((classes.length ? classes[classes.length - 1] + '-' : '') + parts.shift());
+			document.body.className = classes.map(function (cls) { return cls + '-view'; }).join(' ');
+			return;
+		}
 	}
 	
-	// Main page.
-	
+	// No match.
+	document.body.className = '';
+	window.location.hash = '#';
 };
 window.onhashchange();
 
@@ -50,24 +70,48 @@ function toSet(arrays) {
 }
 
 var table = document.querySelector('#courses tbody');
+
+function colorCourseName(name) {
+	return name.replace(/^[A-Z]+/, function (x) { return '<b>' + x + '</b>'; }).replace(/[1-9]\d*/, function (x) { return '<b>' + x + '</b>'; }).replace(/[A-Z]{2}$/, function (school) {
+		var colors = {
+			'HM': 'gold',
+			'PO': 'blue',
+			'CM': 'red',
+			'SC': 'green',
+			'PZ': 'orange'
+		};
+		return colors[school] ? '<b style="color: ' + colors[school] + '">' + school + '</b>' : '<b>' + school + '</b>';
+	});
+}
+
 function listCourses(courses) {
 	console.log(courses);
-	table.innerHTML = courses.map(function (crs) {
-		return crs.sections.map(function (sec) {
+	table.onclick = function (e) {
+		// Get the row that was clicked on.
+		var row = e.target;
+		while (row.tagName !== 'TR' || !row.getAttribute('data-index')) {
+			row = row.parentNode;
+			if (!row)
+				return;
+		}
+		var index = row.getAttribute('data-index').split(' ');
+		var details = document.createElement('tr');
+		details.innerHTML = '<td colspan="8">' + courses[index[0]].abstr + '</td>';
+		
+		if (row.nextSibling)
+			row.parentNode.insertBefore(details, row.nextSibling);
+		else
+			row.parentNode.appendChild(details);
+		console.log();
+	};
+
+	table.innerHTML = courses.map(function (crs, i) {
+		return crs.sections.map(function (sec, j) {
 			var instructors = toSet(sec.meetings.map(function (mtg) { return mtg.instructors; }));
 			
 			return '\
-				<tr>\
-					<td>' + crs.crs_no.replace(/^[A-Z]+/, function (x) { return '<b>' + x + '</b>'; }).replace(/[1-9]\d*/, function (x) { return '<b>' + x + '</b>'; }).replace(/[A-Z]{2}$/, function (school) {
-						var colors = {
-							'HM': 'gold',
-							'PO': 'blue',
-							'CM': 'red',
-							'SC': 'green',
-							'PZ': 'orange'
-						};
-						return colors[school] ? '<b style="color: ' + colors[school] + '">' + school + '</b>' : '<b>' + school + '</b>';
-					}) + "&#8209;" + sec.sec_no + '</td>\
+				<tr data-index="' + i + ' ' + j + '">\
+					<td>' + colorCourseName(crs.crs_no) + "&#8209;" + sec.sec_no + '</td>\
 					<td><b>' + crs.title + '</b>' + (sec.title ? '<br />' + sec.title : '') + '</td>\
 					<td>' + instructors.join('<br />') + '</td>\
 					<td>\
@@ -114,12 +158,22 @@ function listCourses(courses) {
 					</td>\
 					<td>' + sec.beg_date + '<br />' + sec.end_date + '</td>\
 					<td><span style="color: #666">enrolled</span>: ' + sec.reg_num + '<br /><span style="color: #666">max:</span> ' + sec.reg_max + '</td>\
-					<td>' + sec.units + '</td>\
+					<td>' + sec.units.toFixed(2) + '</td>\
 					<td><button>Add</button></td>\
 				</tr>';
 		}).join('');
 	}).join('');
+	
+	// Scroll to the top.
+	window.scroll(0, 0);
 }
+
+document.querySelector('#search').onsubmit = function () {
+	var cat = document.querySelector('#cat').value;
+	var disc = document.querySelector('#disc').value;
+	window.location.hash = '#catalog/' + cat + '/' + disc;
+	return false;
+};
 
 function api(query, callback) {
 	var req = new XMLHttpRequest();
@@ -138,9 +192,3 @@ function api(query, callback) {
 	};
 	req.send(null);
 }
-
-document.querySelector('#search button').onclick = function () {
-	var cat = document.querySelector('#cat').value;
-	var disc = document.querySelector('#disc').value;
-	window.location.hash = '#list/' + cat + '/' + disc;
-};
